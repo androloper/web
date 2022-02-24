95fc0ec76d389fa3ffcf036eed42a978971ca67a
Ramazan Baybörek <ramazanbayborek@gmail.com>
2022-02-24 09:21:55 +0300

    contact page has implemented

--- mostotomasyon.com/src/app/modules/iletisim/iletisim.component.ts
+++ mostotomasyon.com/src/app/modules/iletisim/iletisim.component.ts
@@ -1,4 +1,5 @@
-import { Component, OnInit } from '@angular/core';
+import {Component, OnInit, ViewChild} from '@angular/core';
+import {FormBuilder, FormGroup, NgForm, Validators} from '@angular/forms';
 
 @Component({
   selector: 'app-iletisim',
@@ -7,9 +8,42 @@ import { Component, OnInit } from '@angular/core';
 })
 export class IletisimComponent implements OnInit {
 
-  constructor() { }
+    @ViewChild('supportNgForm') supportNgForm: NgForm;
+    alert: any;
+    supportForm: FormGroup;
+    constructor(private _formBuilder: FormBuilder) { }
 
-  ngOnInit(): void {
-  }
+    ngOnInit(): void {
+        this.supportForm = this._formBuilder.group({
+            name   : ['', Validators.required],
+            email  : ['', [Validators.required, Validators.email]],
+            subject: ['', Validators.required],
+            message: ['', Validators.required]
+        });
+    }
+    clearForm(): void
+    {
+        // Reset the form
+        this.supportNgForm.resetForm();
+    }
 
+    /**
+     * Send the form
+     */
+    sendForm(): void
+    {
+        // Send your form here using an http request
+        console.log('Your message has been sent!');
+        this.alert = {
+            type   : 'success',
+            message: 'Mesajınız bize ulaşmıştır. En kısa zaman içerisinde tarafımızdan dönüş yapılacaktır.'
+        };
+
+        setTimeout(() => {
+            this.alert = null;
+        }, 7000);
+
+        // Clear the form
+        this.clearForm();
+    }
 }
--- mostotomasyon.com/src/app/modules/iletisim/iletisim.module.ts
+++ mostotomasyon.com/src/app/modules/iletisim/iletisim.module.ts
@@ -1,7 +1,13 @@
 import { NgModule } from '@angular/core';
 import { Route, RouterModule } from '@angular/router';
 import { IletisimComponent } from 'app/modules/iletisim/iletisim.component';
-import {CdkScrollableModule} from "@angular/cdk/scrolling";
+import {CdkScrollableModule} from '@angular/cdk/scrolling';
+import {FuseAlertModule} from '../../../@fuse/components/alert';
+import {ReactiveFormsModule} from '@angular/forms';
+import {MatFormFieldModule} from '@angular/material/form-field';
+import {MatInputModule} from '@angular/material/input';
+import {MatButtonModule} from '@angular/material/button';
+import {CommonModule} from "@angular/common";
 
 const iletisimRoutes: Route[] = [
     {
@@ -16,7 +22,13 @@ const iletisimRoutes: Route[] = [
     ],
     imports: [
         RouterModule.forChild(iletisimRoutes),
-        CdkScrollableModule
+        CdkScrollableModule,
+        FuseAlertModule,
+        ReactiveFormsModule,
+        MatFormFieldModule,
+        MatInputModule,
+        MatButtonModule,
+        CommonModule
     ]
 })
 export class IletisimModule
