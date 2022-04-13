14ff590944b11971dfc2993c11c4114c8a3a9243
Ramazan Baybörek <ramazanbayborek@gmail.com>
2022-04-13 12:53:59 +0300

    The third type of datatable has completely implemented

--- web-monitoring/src/app/modules/datatable3/datatable3.component.ts
+++ web-monitoring/src/app/modules/datatable3/datatable3.component.ts
@@ -1,60 +1,78 @@
-import { Component } from '@angular/core';
+import {Component, OnInit} from '@angular/core';
 import { ColumnMode } from '@swimlane/ngx-datatable';
+
+declare const $: any;
 
 @Component({
   selector: 'datatable3',
   templateUrl: './datatable3.component.html',
   styleUrls: ['./datatable3.component.scss']
 })
-export class Datatable3Component{
-    rows = [
-        {
-            name: 'Claudia Oneal',
-            gender: 'female',
-            company: 'SeLouie',
-            name2: 'Claudia Oneal',
-            gender2: 'male',
-            company2: 'Velotivity'
-        },
-        {
-            name: 'Claudine Neal',
-            gender: 'female',
-            company: 'SeLouie',
-            name2: 'Claudine Neal',
-            gender2: 'male',
-            company2: 'Velotivity'
-        },
-        {
-            name: 'Blaudine Neal',
-            gender: 'female',
-            company: 'SeLouie',
-            name2: 'Blaudine Neal',
-            gender2: 'male',
-            company2: 'Velotivity'
-        },
-        {
-            name: 'Cheryl Rice',
-            gender: 'female',
-            company: 'SeLouie',
-            name2: 'Cheryl Rice',
-            gender2: 'male',
-            company2: 'Velotivity'
-        },
-        {
-            name: 'Beryl Nice',
-            gender: 'female',
-            company: 'SeLouie',
-            name2: 'Beryl Nice',
-            gender2: 'male',
-            company2: 'Velotivity'
-        }
-    ];
+export class Datatable3Component implements OnInit{
+    loadingIndicator = true;
+    rows = [];
 
     columns = [{ name: 'Name' }, { name: 'Gender' }, { name: 'Company' }, { name: 'Name2' }, { name: 'Gender2' }, { name: 'Company2' }];
 
     allColumns = [{ name: 'Name' }, { name: 'Gender' }, { name: 'Company' }, { name: 'Name2' }, { name: 'Gender2' }, { name: 'Company2' }];
 
     columnMode = ColumnMode;
+
+    ngOnInit(): void {
+        setTimeout(async ()=> {
+            this.loadingIndicator=false;
+            this.rows = [
+                {
+                    name: 'Claudia Oneal',
+                    gender: 'female',
+                    company: 'SeLouie',
+                    name2: 'Claudia Oneal',
+                    gender2: 'male',
+                    company2: 'Velotivity'
+                },
+                {
+                    name: 'Claudine Neal',
+                    gender: 'female',
+                    company: 'SeLouie',
+                    name2: 'Claudine Neal',
+                    gender2: 'male',
+                    company2: 'Velotivity'
+                },
+                {
+                    name: 'Blaudine Neal',
+                    gender: 'female',
+                    company: 'SeLouie',
+                    name2: 'Blaudine Neal',
+                    gender2: 'male',
+                    company2: 'Velotivity'
+                },
+                {
+                    name: 'Cheryl Rice',
+                    gender: 'female',
+                    company: 'SeLouie',
+                    name2: 'Cheryl Rice',
+                    gender2: 'male',
+                    company2: 'Velotivity'
+                },
+                {
+                    name: 'Beryl Nice',
+                    gender: 'female',
+                    company: 'SeLouie',
+                    name2: 'Beryl Nice',
+                    gender2: 'male',
+                    company2: 'Velotivity'
+                },
+                {
+                    name: 'Keryl Slice',
+                    gender: 'female',
+                    company: 'SeLouie',
+                    name2: 'Keryl Slice',
+                    gender2: 'male',
+                    company2: 'Velotivity'
+                }
+            ];
+        },1500);
+    }
 
     // eslint-disable-next-line @typescript-eslint/explicit-function-return-type
     toggle(col) {
