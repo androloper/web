6e7ddbcd6c6dc68eff334d6172f40499014caa12
Ramazan Baybörek <ramazanbayborek@gmail.com>
2022-04-28 09:28:18 +0300

    Data manipulation for empty columns to get more details

--- web-monitoring/src/app/modules/datatable9/datatable9.component.ts
+++ web-monitoring/src/app/modules/datatable9/datatable9.component.ts
@@ -7,6 +7,7 @@ import {exportDataGrid as exportDataGridToPdf} from 'devextreme/pdf_exporter';
 import {Workbook} from 'exceljs';
 import {exportDataGrid} from 'devextreme/excel_exporter';
 import saveAs from 'file-saver';
+import {distinct} from "rxjs";
 
 @Component({
     selector: 'datatable9',
@@ -17,6 +18,14 @@ import saveAs from 'file-saver';
 export class Datatable9Component implements OnInit {
     @ViewChild(DxDataGridComponent, {static: false}) dataGrid: DxDataGridComponent;
     athletes: Datatable9[];
+    exhibitor: any[];
+    exhibitors: string;
+    countriesInOlympics: any[];
+    countries: string;
+    sportsInOlympics: any[];
+    sports: string;
+    totalMedal=0;
+    avg: string;
     constructor(private service: Datatable9Service) {
     }
 
@@ -24,6 +33,17 @@ export class Datatable9Component implements OnInit {
        this.service.getList('https://raw.githubusercontent.com/androloper/jsons/main/olympic-winners.json')
             .subscribe((data)=>{
                 this.athletes=data;
+                this.exhibitor = [...new Set(data.map(item => item.athlete))];
+                this.exhibitors = this.exhibitor.length.toString();
+                this.countriesInOlympics = [...new Set(data.map(item => item.country))];
+                this.countries = this.countriesInOlympics.length.toString();
+                this.sportsInOlympics = [...new Set(data.map(item => item.sport))];
+                this.sports = this.sportsInOlympics.length.toString();
+                // eslint-disable-next-line @typescript-eslint/prefer-for-of
+                for (let i = 0; i < data.length; i++) {
+                    this.totalMedal += data[i].total;
+                }
+                this.avg = (this.totalMedal/data.length).toFixed(2);
             });
     }
 
